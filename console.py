e886df46e9d6ef2dcd998abcb3c995adb8a83a58
Amna <amna.bashir2@gmail.com>
2021-12-10 12:49:01 +0000

    cities table created

--- console.py
+++ console.py
@@ -1,9 +1,23 @@
 import pdb
+
+from models.city import City
 from models.country import Country
 
+import repositories.city_repository as city_repository
 import repositories.country_repository as country_repository
+
 
 country1= Country('United Kingdom')
 country_repository.save(country1)
 country2=Country('Pakistan')
 country_repository.save(country2)
+
+
+city1=City("Edinburgh", country1)
+city_repository.save(city1)
+city2=City("Glasgow", country1)
+city_repository.save(city2)
+city3=City("Lahore", country2)
+city_repository.save(city3)
+city4=City("Islamabad", country2)
+city_repository.save(city4)
--- repositories/country_repository.py
+++ repositories/country_repository.py
@@ -1,8 +1,10 @@
 from db.run_sql import run_sql
+
 from models.country import Country
+from models.city import City
 
 def save(country):
-    sql="INSERT INTO countries( name ) VALUES (%s) RETURNING id"
+    sql="INSERT INTO countries( name ) VALUES (%s) RETURNING *"
     values = [country.name]
     results = run_sql( sql, values )
     country.id = results[0]['id']

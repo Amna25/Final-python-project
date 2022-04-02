de73ecb5beeb36e440eca99e4531fcb5ce5c4892
Amna Bashir <94113478+Amna25@users.noreply.github.com>
2022-04-02 09:04:43 +0100

    change app to application

--- application.py
+++ application.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template
 from controllers.cities_controller import cities_blueprint
 from controllers.countries_controller import countries_blueprint
 from controllers.destinations_controller import destinations_blueprint
-app = Flask(__name__)
+application = Flask(__name__)
 
-app.register_blueprint(cities_blueprint)
-app.register_blueprint(countries_blueprint)
-app.register_blueprint(destinations_blueprint)
+application.register_blueprint(cities_blueprint)
+application.register_blueprint(countries_blueprint)
+application.register_blueprint(destinations_blueprint)
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
